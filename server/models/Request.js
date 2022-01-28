dd89f41a1f257e52403105d76e33f2676748a3d8
JunZheng007 <jzheng0721@gmail.com>
2022-01-28 16:50:56 -0500

    add request model

---
+++ server/models/Request.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const RequestSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "User",
+  },
+  sitterId: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "User",
+  },
+  startDate: {
+    type: Date,
+    required: true,
+  },
+  endDate: {
+    type: Date,
+    required: true,
+  },
+  accepted: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+  declined: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+  paid: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+});
+
+module.exports = Request = mongoose.model("Request", RequestSchema);
