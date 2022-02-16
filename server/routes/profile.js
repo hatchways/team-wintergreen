48def6c1c9eaf10d44499c1bb4c7a6eb0be37658
JunZheng007 <jzheng0721@gmail.com>
2022-02-15 21:35:06 -0500

    set profile route

--- server/routes/profile.js
+++ server/routes/profile.js
@@ -1,13 +1,10 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const protect = require('../middleware/auth');
-const {
-  editProfile,
-  loadProfile,
-} = require('../controllers/profile');
+const protect = require("../middleware/auth");
+const { editProfile, loadProfile } = require("../controllers/profile");
 
-router.route('/edit').put(protect, editProfile);
+router.route("/edit").put(protect, editProfile);
 
-router.route('/load').get(protect, loadProfile);
+router.route("/load/:userId").get(protect, loadProfile);
 
 module.exports = router;
