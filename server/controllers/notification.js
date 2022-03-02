67c4131fa493c4a7c9c648e4ba8c8596f46812c6
Kevin <kevin.06.huang@gmail.com>
2022-03-01 19:20:53 -0800

    Added change according to comment

--- server/controllers/notification.js
+++ server/controllers/notification.js
@@ -10,6 +10,30 @@ exports.createNotification = asyncHandler(async (req, res) => {
         }
     }));
 });
+exports.createNotification = asyncHandler(async (req, res) => {
+    const { type, description, receivedBy } = JSON.parse(req.body)
+    const userId = req.user.id
+    
+    const profile = Profile.findById(userId);
+    
+    if (!profile) {
+        return res.status(404).send("No Profile Found")
+    }
+    
+    const newNotifcation = await Notification.create({
+        type,
+        description,
+        receivedBy,
+        createdBy: profile._id
+    })
+    
+    return res.status(201).json({
+        success: {
+            notification: newNotification
+        }
+    })
+    
+});
 exports.readNotifications = asyncHandler(async (req, res) => {
     const { id } = req.user;
     Notification
