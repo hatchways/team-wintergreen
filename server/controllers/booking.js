6ed372d088696d233440739d768e3d44f452478d
JunZheng007 <jzheng0721@gmail.com>
2022-03-03 19:44:46 -0500

    fix marge bug

--- server/controllers/booking.js
+++ server/controllers/booking.js
@@ -60,14 +60,10 @@ res.status(200).json({
 // @desc Update booking with approved or decline
 // @access Private
 exports.updateBooking = asyncHandler(async (req, res, next) => {
-  const { status } = req.body
+  const { status } = req.body;
   const booking = await Booking.findByIdAndUpdate(
     req.params.bookingId,
     status,
-    { returnDocument: "after" }
-  );
-    req.params.bookingId,
-    req.body.data,
     { returnDocument: "after" }
   );
 
