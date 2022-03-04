6f08a6205220c4eb96cd57afb284dbf7e1dc232d
JunZheng007 <jzheng0721@gmail.com>
2022-03-03 21:15:52 -0500

    display rating use MUI rating component

--- client/src/components/RequestForm/RequestForm.tsx
+++ client/src/components/RequestForm/RequestForm.tsx
@@ -1,7 +1,6 @@
 import { Star } from '@mui/icons-material';
-import { DatePicker, LocalizationProvider } from '@mui/lab';
-import AdapterDateFns from '@mui/lab/AdapterDateFns';
-import { Autocomplete, Box, Card, Grid, InputLabel, TextField, Typography } from '@mui/material';
+import { DatePicker } from '@mui/lab';
+import { Autocomplete, Box, Card, Grid, InputLabel, Rating, TextField, Typography } from '@mui/material';
 import Button from '@mui/material/Button';
 import CircularProgress from '@mui/material/CircularProgress';
 import { Form, Formik, FormikHelpers } from 'formik';
@@ -39,104 +38,89 @@ const RequestForm = ({ className, profile, handleSubmit }: Props): JSX.Element =
         <Typography variant="h5" sx={{ fontWeight: 'bold' }}>
           ${profile.price}/hr
         </Typography>
-        <Typography className={classes.star}>{getRankInStar(profile.rank)}</Typography>
+        <Rating name="rank" value={profile.rank} readOnly size="large" sx={{ color: 'gold' }} />
         <Grid item>
-          <LocalizationProvider dateAdapter={AdapterDateFns}>
-            <Formik
-              initialValues={{
-                dropInDate: new Date(),
-                dropInTime: getTimes(new Date().getHours())[0],
-                dropOffDate: new Date(),
-                dropOffTime: getTimes(new Date().getHours())[1],
-              }}
-              onSubmit={handleSubmit}
-            >
-              {(props) => (
-                <Form className={classes.form}>
-                  <Box display="flex" flexDirection="column">
-                    <InputLabel className={classes.label} sx={{ fontWeight: 700, color: '#000' }}>
-                      Drop in
-                    </InputLabel>
-                    <Box display="flex">
-                      <DatePicker
-                        onChange={(value) => props.setFieldValue('dropInDate', value)}
-                        value={props.values.dropInDate}
-                        renderInput={(params) => <TextField sx={{ width: '60%' }} {...params} />}
-                      />
-                      <Autocomplete
-                        onChange={(event, value) => {
-                          props.setFieldValue('dropInTime', value);
-                        }}
-                        defaultValue={props.values.dropInTime}
-                        disableClearable
-                        options={getTimes(
-                          props.values.dropInDate.toDateString() === today.toDateString()
-                            ? props.values.dropInDate.getHours()
-                            : 0,
-                        )}
-                        renderInput={(params) => <TextField {...params} />}
-                        sx={{ width: '40%' }}
-                      />
-                    </Box>
+          <Formik
+            initialValues={{
+              dropInDate: new Date(),
+              dropInTime: getTimes(new Date().getHours())[0],
+              dropOffDate: new Date(),
+              dropOffTime: getTimes(new Date().getHours())[1],
+            }}
+            onSubmit={handleSubmit}
+          >
+            {(props) => (
+              <Form className={classes.form}>
+                <Box display="flex" flexDirection="column">
+                  <InputLabel className={classes.label} sx={{ fontWeight: 700, color: '#000' }}>
+                    Drop in
+                  </InputLabel>
+                  <Box display="flex">
+                    <DatePicker
+                      onChange={(value) => props.setFieldValue('dropInDate', value)}
+                      value={props.values.dropInDate}
+                      renderInput={(params) => <TextField sx={{ width: '60%' }} {...params} />}
+                    />
+                    <Autocomplete
+                      onChange={(event, value) => {
+                        props.setFieldValue('dropInTime', value);
+                      }}
+                      defaultValue={props.values.dropInTime}
+                      disableClearable
+                      options={getTimes(
+                        props.values.dropInDate.toDateString() === today.toDateString()
+                          ? props.values.dropInDate.getHours()
+                          : 0,
+                      )}
+                      renderInput={(params) => <TextField {...params} />}
+                      sx={{ width: '40%' }}
+                    />
                   </Box>
-                  <Box display="flex" flexDirection="column" marginTop={3}>
-                    <InputLabel className={classes.label} sx={{ fontWeight: 700, color: '#000' }}>
-                      Drop Off
-                    </InputLabel>
-                    <Box display="flex">
-                      <DatePicker
-                        onChange={(value) => props.setFieldValue('dropOffDate', value)}
-                        value={props.values.dropOffDate}
-                        renderInput={(params) => <TextField sx={{ width: '60%' }} {...params} />}
-                      />
-                      <Autocomplete
-                        onChange={(event, value) => props.setFieldValue('dropOffTime', value)}
-                        defaultValue={props.values.dropOffTime}
-                        disableClearable
-                        options={getTimes(
-                          props.values.dropOffDate.toDateString() === today.toDateString()
-                            ? props.values.dropOffDate.getHours() + 1
-                            : 0,
-                        )}
-                        renderInput={(params) => <TextField {...params} />}
-                        sx={{ width: '40%' }}
-                      />
-                    </Box>
+                </Box>
+                <Box display="flex" flexDirection="column" marginTop={3}>
+                  <InputLabel className={classes.label} sx={{ fontWeight: 700, color: '#000' }}>
+                    Drop Off
+                  </InputLabel>
+                  <Box display="flex">
+                    <DatePicker
+                      onChange={(value) => props.setFieldValue('dropOffDate', value)}
+                      value={props.values.dropOffDate}
+                      renderInput={(params) => <TextField sx={{ width: '60%' }} {...params} />}
+                    />
+                    <Autocomplete
+                      onChange={(event, value) => props.setFieldValue('dropOffTime', value)}
+                      defaultValue={props.values.dropOffTime}
+                      disableClearable
+                      options={getTimes(
+                        props.values.dropOffDate.toDateString() === today.toDateString()
+                          ? props.values.dropOffDate.getHours() + 1
+                          : 0,
+                      )}
+                      renderInput={(params) => <TextField {...params} />}
+                      sx={{ width: '40%' }}
+                    />
                   </Box>
+                </Box>
 
-                  <Box textAlign="center" marginTop={5}>
-                    <Button
-                      type="submit"
-                      size="large"
-                      variant="contained"
-                      color="primary"
-                      className={classes.submit}
-                      disableElevation
-                    >
-                      {props.isSubmitting ? <CircularProgress style={{ color: 'white' }} /> : 'Send request'}
-                    </Button>
-                  </Box>
-                </Form>
-              )}
-            </Formik>
-          </LocalizationProvider>
+                <Box textAlign="center" marginTop={5}>
+                  <Button
+                    type="submit"
+                    size="large"
+                    variant="contained"
+                    color="primary"
+                    className={classes.submit}
+                    disableElevation
+                  >
+                    {props.isSubmitting ? <CircularProgress style={{ color: 'white' }} /> : 'Send request'}
+                  </Button>
+                </Box>
+              </Form>
+            )}
+          </Formik>
         </Grid>
       </Grid>
     </Card>
   );
-};
-
-const getRankInStar = (rank: number): JSX.Element => {
-  const stars = [];
-  for (let i = 0; i < 5; i++) {
-    if (rank >= 1) {
-      stars.push(<Star />);
-      rank -= 1;
-    } else {
-      stars.push(<Star color="disabled" />);
-    }
-  }
-  return <Box>{stars.map((value) => value)}</Box>;
 };
 
 export default RequestForm;
