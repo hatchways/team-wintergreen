7f7638a6098fcc51480d1f85c91db5806cc75d9b
Kevin <kevin.06.huang@gmail.com>
2022-05-16 02:07:47 -0700

    Added all

--- client/src/pages/Dashboard/SearchDisplay.tsx
+++ client/src/pages/Dashboard/SearchDisplay.tsx
@@ -3,6 +3,7 @@ import useStyle from './useStyles';
 import React, { useState } from 'react';
 import ProfileCard from './ProfileCard';
 import { Profile } from '../../interface/Profile';
+import { DragDropContainer, DropTarget } from 'react-drag-drop-container-typescript';
 interface Props {
   profiles: Profile[];
 }
@@ -19,11 +20,15 @@ const SearchDisplay = ({ profiles }: Props) => {
       alignItems="center"
       justifyContent="center"
     >
-      {profiles.map((card, index) => (
-        <Box key={index} className={classes.card}>
-          <ProfileCard name={`${card}`} />
-        </Box>
-      ))}
+      <DropTarget targetKey="foo">
+        {profiles.map((card, index) => (
+          <DragDropContainer key={index} targetKey="foo">
+            <Box key={index} className={classes.card}>
+              <ProfileCard name={`${card}`} />
+            </Box>
+          </DragDropContainer>
+        ))}
+      </DropTarget>
     </Grid>
   );
 };
